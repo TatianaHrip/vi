f7dd078ec8d9f496df921230ba73f3c830c1078b
Tatyana Hripkova <tkhripkova@bk.ru>
2020-11-20 22:28:01 +0200

    second commit

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int balance = 100;
+        int replenished = 6500;
+        int bonus;
+        if (replenished >= 9000) {
+            bonus = balance + replenished  / 100;
+        } else {
+            bonus = balance + replenished;
+        }
+        System.out.println(bonus);
     }
-}
+        }
